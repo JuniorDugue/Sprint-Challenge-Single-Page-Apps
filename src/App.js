3c4688ea3d049a5fbb4629bd08acd0b02fbafb74
Junior Dugue <jrdugue@gmail.com>
2019-09-16 20:03:24 -0400

    fixed the bug in episodelist component with my hook call

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 // import EpisodeCard from "./components/EpisodeCard";
-// import EpisodeList from "./components/EpisodeList";
-import CharacterList from "./components/CharacterList";
-import {Route} from "react-router-dom";
+import EpisodeList from "./components/EpisodeList";
+// import CharacterList from "./components/CharacterList";
+// import {Route} from "react-router-dom";
 export default function App() {
   return (
     <main>
       <Header />
       <TabNav />
-      <Route exact path="/characterList" component={CharacterList}/>
+      {/* <Route exact path="/characterList" component={CharacterList}/> */}
       {/* <EpisodeCard/> */}
-      {/* <EpisodeList/> */}
-      <CharacterList/>
+      <EpisodeList/>
+      {/* <CharacterList/> */}
     </main>
   );
 }
--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -5,21 +5,20 @@ import EpisodeCard from "./EpisodeCard";
 
 const BASE_URL = "https://rickandmortyapi.com/api/episode/";
 
-const [getEpisodes, setGetEpisodes] = useState([{
-  id: "",
-  name: "",
-  air_date: "",
-  episode: "", 
-  characters: []
-}])
-
-useEffect(() => {
-  axios.get(BASE_URL)
-  .then(res => setGetEpisodes(res.data.results))
-  .catch(err => console.log(err))
-})
-
 function EpisodeList(){
+  const [getEpisodes, setGetEpisodes] = useState([{
+    id: "",
+    name: "",
+    air_date: "",
+    episode: "", 
+    characters: [],
+  }])
+  
+  useEffect(() => {
+    axios.get(BASE_URL)
+    .then(res => setGetEpisodes(res.data.results))
+    .catch(err => console.log(err))
+  })
   return(
     <div className="episodelist">
       <h1>EpisodeList</h1>
